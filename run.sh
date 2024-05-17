e756b470c464e2f9ce2e582b5c4c1b54fa641631
Dương Anh Kiệt <43516744+ffyyytt@users.noreply.github.com>
2024-05-17 17:37:49 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+# L'argument '-l' est indispensable pour bénéficier des directives de votre .bashrc
+ 
+# On peut éventuellement placer ici les commentaires SBATCH permettant de définir les paramètres par défaut de lancement :
+#SBATCH --gres gpu:1
+#SBATCH --time 1-23:50:00
+#SBATCH --cpus-per-task 9
+#SBATCH --mem-per-cpu 3G
+#SBATCH --mail-type FAIL,END
+
+conda activate tf2gpu
+python3 main.py
